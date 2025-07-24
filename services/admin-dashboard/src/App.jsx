4be457d04efed16cbcb34bed1caba86695d272be
SkyBehind <aaron@magicunicorn.tech>
2025-07-23 22:57:13 -0400

    Fix React build errors
    - Remove non-existent Storage page import
    - Remove ThemeProvider that wasn't created
    - Add missing System page to routes
    - Fix route structure alignment
    
    Built by Magic Unicorn Unconventional Technology & Stuff Inc

--- services/admin-dashboard/src/App.jsx
+++ services/admin-dashboard/src/App.jsx
@@ -3,31 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Models from './pages/Models';
 import Services from './pages/Services';
+import System from './pages/System';
 import Network from './pages/Network';
-import Storage from './pages/Storage';
 import Settings from './pages/Settings';
 import Layout from './components/Layout';
-import { ThemeProvider } from './contexts/ThemeContext';
 import { SystemProvider } from './contexts/SystemContext';
 
 function App() {
   return (
-    <ThemeProvider>
-      <SystemProvider>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/models" element={<Models />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/network" element={<Network />} />
-              <Route path="/storage" element={<Storage />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </Layout>
-        </Router>
-      </SystemProvider>
-    </ThemeProvider>
+    <SystemProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/models" element={<Models />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/system" element={<System />} />
+            <Route path="/network" element={<Network />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </SystemProvider>
   );
 }
 
